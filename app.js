903afb2fb41717647b1dc539416354c13adc8a83
FelipeD4ntas <felipeicedantas@gmail.com>
2022-05-31 21:22:22 -0300

    up

--- app.js
+++ app.js
@@ -1,50 +1,14 @@
-/*
-  07
-
-  - No index.html, comente a div com a classe "container" que contém a tabela;
-  - Descomente: 
-    - A <div> com a classe "container" abaixo da div que você acabou de 
-      comentar;
-    - A <link> que importa o style.css;
-  - Construa uma aplicação de conversão de moedas. O HTML e CSS são os que 
-    você está vendo no browser (após salvar os arquivos);
-  - Você poderá modificar a marcação e estilos da aplicação depois. No momento, 
-    concentre-se em executar o que descreverei abaixo;
-    - Quando a página for carregada: 
-      - Popule os <select> com tags <option> que contém as moedas que podem ser
-        convertidas. "BRL" para real brasileiro, "EUR" para euro, "USD" para 
-        dollar dos Estados Unidos, etc.
-      - O option selecionado por padrão no 1º <select> deve ser "USD" e o option
-        no 2º <select> deve ser "BRL";
-      - O parágrafo com data-js="converted-value" deve exibir o resultado da 
-        conversão de 1 USD para 1 BRL;
-      - Quando um novo número for inserido no input com 
-        data-js="currency-one-times", o parágrafo do item acima deve atualizar 
-        seu valor;
-      - O parágrafo com data-js="conversion-precision" deve conter a conversão 
-        apenas x1. Exemplo: 1 USD = 5.0615 BRL;
-      - O conteúdo do parágrafo do item acima deve ser atualizado à cada 
-        mudança nos selects;
-      - O conteúdo do parágrafo data-js="converted-value" deve ser atualizado à
-        cada mudança nos selects e/ou no input com data-js="currency-one-times";
-      - Para que o valor contido no parágrafo do item acima não tenha mais de 
-        dois dígitos após o ponto, você pode usar o método toFixed: 
-        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-    - Para obter as moedas com os valores já convertidos, use a Exchange rate 
-      API: https://www.exchangerate-api.com/;
-      - Para obter a key e fazer requests, você terá que fazer login e escolher
-        o plano free. Seus dados de cartão de crédito não serão solicitados.
-  
-  PS: o desafio aqui é você implementar essa aplicação sozinho(a) e enviá-la 
-  para análise antes de ver as próximas aulas, ok? =)
-*/
-
-
 const chaveAPI = '30ee27cf65ba91b18f740b1e';
 const selectEsquerdo = document.querySelector('[data-js="currency-one"]');
 const selectDireito = document.querySelector('[data-js="currency-two"]');
-const resultadoConversao = document.querySelector('[data-js="converted-value]');
+const quantidadeMoeda = document.querySelector('[data-js="currency-one-times"]');
+const resultadoConversao = document.querySelector('[data-js="converted-value"]');
+const resultadoConversaoBruta = document.querySelector('[data-js="conversion-precision"]');
 
+let valorSelectEsquerdo = null;
+let valorSelectDireito = null;
+let nomeSelectEsquerdo = null;
+let nomeSelectDireito = null;
 
 async function obterMoedas() {
   try {
@@ -66,11 +30,43 @@ function preencherSelect(select, nomeMoedas, valorMoedas, moedaPadrao) {
     moeda === moedaPadrao 
       ? select.innerHTML += `<option value="${valorMoedas[index]}" selected>${moeda}</option>` 
       : select.innerHTML += `<option value="${valorMoedas[index]}">${moeda}</option>`
-  })
-}
+  });
+};
 
-function converterMoedas(event) {
- console.log(event.target.options[event.target.selectedIndex].value);
+function obterValorPadraoMoeda(select) {
+  return select.options[select.selectedIndex].value
+};
+
+function obterNomePadraoMoeda(select) {
+  return select.options[select.selectedIndex].text
+};
+
+function obterValorMoeda(event) {
+  const quantidadeMoedaParaConversao = quantidadeMoeda.value;
+
+  valorSelectDireito = obterValorPadraoMoeda(selectDireito);
+  valorSelectEsquerdo = obterValorPadraoMoeda(selectEsquerdo);
+  nomeSelectDireito = obterNomePadraoMoeda(selectDireito);
+  nomeSelectEsquerdo = obterNomePadraoMoeda(selectEsquerdo);
+
+  if (event.target === selectEsquerdo) {
+    valorSelectEsquerdo = event.target.options[event.target.selectedIndex].value;
+    nomeSelectEsquerdo = event.target.options[event.target.selectedIndex].text;
+  }
+  
+  if (event.target === selectDireito) {
+    valorSelectDireito = event.target.options[event.target.selectedIndex].value;
+    nomeSelectDireito = event.target.options[event.target.selectedIndex].text;
+  }
+  
+  converterMoedas(valorSelectEsquerdo, valorSelectDireito, nomeSelectEsquerdo, nomeSelectDireito, quantidadeMoedaParaConversao);
+};
+
+function converterMoedas(valorOptionUm, valorOptionDois, nomeMoedaOptionUm, nomeMoedaOptionDois, quantidadeMoedaParaConversao) {
+  const valorBruto = (valorOptionUm * valorOptionDois).toFixed(2);
+  const valorConversao = (valorBruto * quantidadeMoedaParaConversao).toFixed(2);
+  resultadoConversaoBruta.innerHTML = `1 ${nomeMoedaOptionUm} = ${valorBruto} ${nomeMoedaOptionDois}`;
+  resultadoConversao.innerHTML = valorConversao;
 };
 
 async function obterDadosMoedas() {
@@ -82,9 +78,18 @@ async function obterDadosMoedas() {
 
   preencherSelect(selectEsquerdo, nomeMoedas, valorMoedas,'USD');
   preencherSelect(selectDireito, nomeMoedas, valorMoedas, 'BRL');
-}
 
-selectDireito.addEventListener('input', converterMoedas);
-selectEsquerdo.addEventListener('input', converterMoedas);
+  const valorPadraoSelectDireito = obterValorPadraoMoeda(selectDireito);
+  const valorPadraoSelectEsquerdo = obterValorPadraoMoeda(selectEsquerdo);
+  const nomePadraoSelectDireito =  obterNomePadraoMoeda(selectDireito);
+  const nomePadraoSelectEsquerdo =  obterNomePadraoMoeda(selectEsquerdo);
+  const quantidadePadraoMoedaParaConversao = 1;
 
-obterDadosMoedas()
+  converterMoedas(valorPadraoSelectEsquerdo, valorPadraoSelectDireito, nomePadraoSelectEsquerdo, nomePadraoSelectDireito, quantidadePadraoMoedaParaConversao);
+};
+
+selectEsquerdo.addEventListener('input', obterValorMoeda);
+selectDireito.addEventListener('input', obterValorMoeda);
+quantidadeMoeda.addEventListener('input', obterValorMoeda);
+
+obterDadosMoedas();
